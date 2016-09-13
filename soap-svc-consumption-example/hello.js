d6f137f91d7c26bd989a797e930787efb1921801
Haris Beg <haris.beg@gmail.com>
2016-09-13 13:11:12 -0700

    delete our call to `res.json` for now
    This is used to send a response back to the client which sent the
    request (in our case the app). We’ll come back to this later.

--- soap-svc-consumption-example/hello.js
+++ soap-svc-consumption-example/hello.js
@@ -8,14 +8,10 @@ function helloRoute() {
   //hello.use(bodyParser());
 
 
-  // ALL REST endpoint - query params may or may not be populated
   hello.all('/', function(req, res) {
-    console.log(new Date(), 'In hello route ALL / req.query=', req.query);
+    console.log(new Date(), 'In hello route / req.query=', req.query);
     var barcode = req.query.barcode || req.body.barcode;
     var wsdlUrl = 'http://www.searchupc.com/service/UPCSearch.asmx?wsdl';
-
-    // see http://expressjs.com/4x/api.html#res.json
-    res.json({msg: 'Hello ' + world});
   });
 
   return hello;
