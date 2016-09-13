560983d52636f9eccc39aade71536c32944e3376
Haris Beg <haris.beg@gmail.com>
2016-09-13 13:21:28 -0700

    Call the SOAP service
    https://gist.github.com/cianclarke/5a17fa8e4554c9c3b88f#file-hello-js

--- soap-svc-consumption-example/hello.js
+++ soap-svc-consumption-example/hello.js
@@ -1,3 +1,4 @@
+var soap = require('soap');
 var express = require('express');
 //var bodyParser = require('body-parser');
 var cors = require('cors');
@@ -12,6 +13,24 @@ function helloRoute() {
     console.log(new Date(), 'In hello route / req.query=', req.query);
     var barcode = req.query.barcode || req.body.barcode;
     var wsdlUrl = 'http://www.searchupc.com/service/UPCSearch.asmx?wsdl';
+    soap.createClient(wsdlUrl, function(err, soapClient){
+        // we now have a soapClient - we also need to make sure there's no `err` here. 
+        if (err) {
+            return res.status(500).json(err);
+        }
+
+        soapClient.GetProduct({
+            upc : barcode,
+            accesstoken : '924646BB-A268-4007-9D87-2CE3084B47BC'
+        }, function (err, result) {
+            if (err) {
+                return res.status(500).json(err);
+            }
+            else {
+                return res.json(result);
+            }
+        });
+    });
   });
 
   return hello;
