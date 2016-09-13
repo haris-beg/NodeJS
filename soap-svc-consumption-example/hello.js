01e73c3bcc4739cbcd889b9bb2dbc6d2bc651212
Haris Beg <haris.beg@gmail.com>
2016-09-13 12:35:28 -0700

    Modify the GET route to make it handle all HTTP verbs

--- soap-svc-consumption-example/hello.js
+++ soap-svc-consumption-example/hello.js
@@ -5,11 +5,11 @@ var cors = require('cors');
 function helloRoute() {
   var hello = new express.Router();
   hello.use(cors());
-  hello.use(bodyParser());
+  //hello.use(bodyParser());
 
 
-  // GET REST endpoint - query params may or may not be populated
-  hello.get('/', function(req, res) {
+  // ALL REST endpoint - query params may or may not be populated
+  hello.all('/', function(req, res) {
     console.log(new Date(), 'In hello route GET / req.query=', req.query);
     var world = req.query && req.query.hello ? req.query.hello : 'World';
 
