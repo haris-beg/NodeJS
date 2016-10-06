bf9e67acf7eeac22a12f0fc9a79c09a1c9c9c6e4
Haris Beg <haris.beg@gmail.com>
2016-10-06 08:58:36 -0700

    Using Command Line Args with process.argv

--- read-treehouse-user-api/app.js
+++ read-treehouse-user-api/app.js
@@ -2,7 +2,7 @@
  * Created by r625361 on 10/5/2016.
  */
 var profile = require("./profile");
-var users = ["chalkers", "harisbeg"];
-
+//Usage: node app harisbeg chalkers davemcfarland ...
+var users = process.argv.slice(2);
 users.forEach(profile.get);
 
