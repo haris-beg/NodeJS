b3909bf8ea9d33e76f07297c43d0d51b8f2d2bc3
Haris Beg <haris.beg@gmail.com>
2016-10-05 19:52:55 -0700

    Removing commented code

--- read-treehouse-user-api/app.js
+++ read-treehouse-user-api/app.js
@@ -6,31 +6,28 @@
 var https = require('https');
 var username = "harisbeg";
 
+//Print out message
 function printMessage(username, badgeCount, points) {
     var message = username + ' has ' + badgeCount + ' total badge(s) and ' + points + ' points in JavaScript';
     console.log(message);
 }
 
+// Print out error messages
+
 //Connect to the API URL (http://teamtreehouse.com/username.json)
 var request = https.get("https://teamtreehouse.com/" + username + ".json", function (response) {
-    //console.dir(response);
-    //console.log(response.statusCode);
     var body = "";
     //Read the data
     response.on('data', function (chunk) {
         body += chunk;
-        //console.log('Response Body : ' + chunk);
     });
     response.on('end', function () {
         var profile = JSON.parse(body);
         printMessage(username, profile.badges.length, profile.points.JavaScript);
-        //console.dir(profile);
-        //console.log(body);
-        //console.log(typeof body);
     });
+    //Parse the data
+    //Print the data
 });
-//Parse the data
-//Print the data
 
 request.on("error", function (error) {
     console.error(error.message);
