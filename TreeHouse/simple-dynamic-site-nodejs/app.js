060619d6ea70c27897ebc2b6156bce67efae2f75
Haris Beg <haris.beg@gmail.com>
2016-10-07 08:20:14 -0700

    User Route

--- TreeHouse/simple-dynamic-site-nodejs/app.js
+++ TreeHouse/simple-dynamic-site-nodejs/app.js
@@ -9,6 +9,7 @@ var router = require("./router");
 var http = require("http");
 http.createServer(function (request, response) {
     router.home(request, response);
+    router.user(request, response);
 }).listen(3000);
 console.log('Server running at http://localhost:3000');
 
--- TreeHouse/simple-dynamic-site-nodejs/router.js
+++ TreeHouse/simple-dynamic-site-nodejs/router.js
@@ -16,11 +16,22 @@ function home(request, response) {
 }
 
 // Handle HTTP route for GET/:username ie, /harisbeg
+function user(request, response) {
     // if url = "/..."
+    var username = request.url.replace("/", "");
+    if (username.length > 0) {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write("Header\n");
+        response.write(username + "\n");
+        response.end('Footer\n');
+
         // get JSON from treehouse.com
         // on "end"
         // show profile
         // on "error"
         //show error
+    }
+}
 
 module.exports.home = home;
+module.exports.user = user;
