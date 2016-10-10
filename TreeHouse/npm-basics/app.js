612df385c58b800070b351b46542add2a66e7853
Haris Beg <haris.beg@gmail.com>
2016-10-09 19:28:39 -0700

    npm install colors --save

--- TreeHouse/npm-basics/app.js
+++ TreeHouse/npm-basics/app.js
@@ -1,9 +1,10 @@
 var bcrypt = require('bcryptjs');
+var colors = require('colors');
 
 var unsecurePlainTextPassword = "password";
 
 bcrypt.genSalt(10, function(err, salt) {
     bcrypt.hash(unsecurePlainTextPassword, salt, function(err, hash) {
-        console.log("hashed password = " + hash);
+        console.log("hashed password = " + hash.green);
     });
 });
