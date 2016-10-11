669373d1c82abbee960a7b957d0ac8833fc35621
Haris Beg <haris.beg@gmail.com>
2016-10-10 17:30:33 -0700

    Responses and the Response Object

--- TreeHouse/express-basics/src/app.js
+++ TreeHouse/express-basics/src/app.js
@@ -11,8 +11,14 @@ app.get('/', function (req, res) {
 
 app.get('/blog/:title?', function (req, res) {
     var title = req.params.title;
-    var post = posts[title];
-    res.send(post);
+    if (title === undefined) {
+        res.status(503);
+        res.send("This page is under construction!");
+    }
+    else {
+        var post = posts[title];
+        res.send(post);
+    }
 });
 
 app.listen(3000, function () {
