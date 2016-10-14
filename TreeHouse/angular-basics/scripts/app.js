1147a74419d36bacd20c63d91334e5b91f7ce210
Haris Beg <haris.beg@gmail.com>
2016-10-14 16:57:03 -0700

    Understanding Scope In Angular

--- TreeHouse/angular-basics/scripts/app.js
+++ TreeHouse/angular-basics/scripts/app.js
@@ -1,6 +1,21 @@
 angular.module("todoListApp", [])
     .controller('mainCtrl', function ($scope) {
         $scope.helloWorld = function () {
-            console.log("Hello there! This is the helloWorld controller function!");
+            console.log("Hello there! This is the main controller function!");
         }
+    })
+
+    .controller('coolCtrl', function ($scope) {
+        $scope.whoAmI = function () {
+            console.log("hello there, this is the coolCtrl function!");
+        }
+
+        $scope.helloWorld = function () {
+            console.log("This is not the main controller!");
+        }
+    })
+
+    .controller('imASibling', function ($scope) {
+        $scope.foobar = 12345;
+
     });
