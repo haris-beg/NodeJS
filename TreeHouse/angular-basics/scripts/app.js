37eb84421011c83c24860036e15f6656b45b1aeb
Haris Beg <haris.beg@gmail.com>
2016-10-15 07:15:19 -0700

    Test to see whether a scope variable is accesible in the html
    You can do this by writing an angular expression like {{todos}} right
    inside the html

--- TreeHouse/angular-basics/scripts/app.js
+++ TreeHouse/angular-basics/scripts/app.js
@@ -1,9 +1,18 @@
 angular.module("todoListApp", [])
     .controller('mainCtrl', function ($scope) {
+
         $scope.helloWorld = function () {
             console.log("Hello there! This is the main controller function!");
-        }
-    })
+        };
 
+        $scope.todos = [
+            {"name": "clean the house"},
+            {"name": "water the lawn"},
+            {"name": "mow the yard"},
+            {"name": "pay bills"},
+            {"name": "jog"},
+            {"name": "elliptical"}
+        ];
 
     });
+
