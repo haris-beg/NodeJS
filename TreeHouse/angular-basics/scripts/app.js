b58c42bc8a2be68a7c9d9f9a7a5e9b4daf08193d
Haris Beg <haris.beg@gmail.com>
2016-10-17 07:16:10 -0700

    more housekeeping ... code cleanup

--- TreeHouse/angular-basics/scripts/app.js
+++ TreeHouse/angular-basics/scripts/app.js
@@ -1,10 +1,6 @@
 angular.module("todoListApp", [])
     .controller('mainCtrl', function ($scope, dataService) {
 
-        $scope.learningNgChange = function () {
-            console.log("An input changed!");
-        };
-
         $scope.helloWorld = dataService.helloWorld;
 
         dataService.getTodos(function(response) {
