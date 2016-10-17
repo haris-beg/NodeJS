44d70c68215138a89af327d6eb1262ceb663a6f9
Haris Beg <haris.beg@gmail.com>
2016-10-16 18:45:39 -0700

    Housekeeping - refactoring todo's into their own file

--- TreeHouse/angular-basics/scripts/app.js
+++ TreeHouse/angular-basics/scripts/app.js
@@ -7,15 +7,6 @@ angular.module("todoListApp", [])
             console.log("An input changed!");
         };
 
-        $scope.todos = [
-            {"name": "clean the house"},
-            {"name": "water the lawn"},
-            {"name": "mow the yard"},
-            {"name": "pay bills"},
-            {"name": "jog"},
-            {"name": "elliptical"}
-        ];
-
     })
     .service('dataService', function () {
 
