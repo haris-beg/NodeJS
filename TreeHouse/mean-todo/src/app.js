410746cc7bbdc056377ae6bcf4506c1264945e2e
Haris Beg <haris.beg@gmail.com>
2016-10-18 21:01:37 -0700

    some housekeeping ...

--- TreeHouse/mean-todo/src/app.js
+++ TreeHouse/mean-todo/src/app.js
@@ -11,7 +11,13 @@ var router = express.Router();
 
 router.get('/todos', function (req, res) {
     res.json({todos: []});
-})
+});
+
+// TODO: add POST route to create new entries
+
+// TODO: add PUT route to update existing entries
+
+// TODO: add DELETE route to delete entries
 
 app.use('/api', router);
 
