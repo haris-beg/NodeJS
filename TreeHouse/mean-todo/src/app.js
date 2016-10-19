1c356ae58b168b14cc52219871d711c38f5ad144
Haris Beg <haris.beg@gmail.com>
2016-10-18 20:49:51 -0700

    Routing - add a route for the '/api/todos' endpoint

--- TreeHouse/mean-todo/src/app.js
+++ TreeHouse/mean-todo/src/app.js
@@ -7,6 +7,10 @@ var app = express();
 // serve the static (angular app) file from the public folder
 app.use('/', express.static('public'));
 
+app.get('/api/todos', function (req, res) {
+    res.send("These are the todos!");
+})
+
 app.listen(3000, function () {
     console.log("The server is running on port 3000!");
 });
