1f64e1cacf64fb5818c4196b0b035a7f4e78862a
Haris Beg <haris.beg@gmail.com>
2016-10-21 14:43:55 -0700

    Using Angular-wrapped JavaScript functions
    https://teamtreehouse.com/library/using-angularwrapped-javascript-apis
    Angular’s digest cycle does much in the way of 2-way data binding and
    tries really hard to keep the DOM in sync with our JavaScript code. As a
    result, we’ll need to use caution when applying some basic JavaScript
    methods.

--- TreeHouse/mean-todo/app/scripts/controllers/main.js
+++ TreeHouse/mean-todo/app/scripts/controllers/main.js
@@ -3,7 +3,18 @@
 var angular = require('angular');
 
 angular.module('todoListApp')
-.controller('mainCtrl', function($scope, dataService){
+.controller('mainCtrl', function($scope, $log, $interval, dataService){
+
+  $scope.seconds = 0;
+
+  $scope.counter = function () {
+    $scope.seconds++;
+    $log.log($scope.seconds + ' seconds have passed!');
+    $log.warn($scope.seconds + ' seconds have passed!');
+    $log.error($scope.seconds + ' seconds have passed!');
+  };
+
+  $interval($scope.counter, 1000, 10);
   
   dataService.getTodos(function(response){
     var todos = response.data.todos;
